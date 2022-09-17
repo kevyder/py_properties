0105bf4062f0b1b4d6a69244c301477482beb3a0
Kevin Rodríguez <kyrodriguez97@gmail.com>
2022-09-16 22:55:18 -0500

    Database connection

---
+++ app/db_config.py
@@ -0,0 +1,28 @@
+import os
+
+import mysql.connector
+from dotenv import load_dotenv
+from mysql.connector import Error
+
+load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_SCHEMA = os.getenv("MYSQL_SCHEMA")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+
+try:
+    connection = mysql.connector.connect(
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_SCHEMA,
+        port=MYSQL_PORT
+    )
+
+    if connection.is_connected():
+        session = connection.cursor()
+        print("Connected to MySQL database")
+except Error as e:
+    print("Error while connecting to MySQL", e)
